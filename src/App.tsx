ea08e604609c212f955f9863d23b42cde52f14c4
Md Yeasin <mdyeasinhn@gmail.com>
2025-01-01 00:19:51 +0600

    increment decrement with redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment } from './redux/features/counter/counterSlice';
 
 function App() {
 
+  const dispatch = useDispatch();
+  const {count} = useSelector((state) => state.counter)
 
+  const handlerIncrement = () =>{
+    dispatch(increment());
+  }
+  const handlerdecrement = () =>{
+    dispatch(decrement());
+  }
   return (
     <>
 
       <div >
       <h1>Counter with redux </h1>
-      <button>Increment</button>
-      <button>0</button>
-      <button>Decrement</button>
+      <button onClick={handlerIncrement}>Increment</button>
+      <button>{count}</button>
+      <button onClick={handlerdecrement}>Decrement</button>
       </div>
     </>
   )
