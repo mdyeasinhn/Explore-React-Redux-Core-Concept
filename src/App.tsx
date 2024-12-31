a5db88016df487c6443938726ae435696cf254d5
Md Yeasin <mdyeasinhn@gmail.com>
2024-12-31 23:30:56 +0600

    initial react-redux project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+
+      <div >
+      <h1>Counter with redux </h1>
+      <button>Increment</button>
+      <button>0</button>
+      <button>Decrement</button>
+      </div>
+    </>
+  )
+}
+
+export default App
